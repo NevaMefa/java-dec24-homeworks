9d6240db6baaed4581db2c1c5e260197512a6bf2
Natali Korban <natafuuuck@gmail.com>
2025-01-11 16:14:27 +0300

    add world

--- src/main/java/ru/otus/java/basic/homeworks/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/Application.java
@@ -3,5 +3,6 @@ package ru.otus.java.basic.homeworks;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("World");
     }
 }
