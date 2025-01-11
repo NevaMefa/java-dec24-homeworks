95df57e71166b7a21f296132112f0886dec5ad14
Natali Korban <natafuuuck@gmail.com>
2025-01-11 16:19:13 +0300

    add int a

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/AppHw3.java
@@ -0,0 +1,8 @@
+package ru.otus.java.basic.homeworks.homework3;
+
+public class AppHw3 {
+    public static void main(String[] args) {
+        int a =4;
+        System.out.println(a + 1);
+    }
+}
