9806bd5db5113d69faf52fe82d910c85c52aa2c5
Natali Korban <natafuuuck@gmail.com>
2025-01-15 23:43:34 +0300

    Добавлен возможность вызова конкретного метода

--- src/main/java/ru/otus/java/basic/homeworks/homework3/AppHw3.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/AppHw3.java
@@ -1,12 +1,30 @@
 package ru.otus.java.basic.homeworks.homework3;
 
+import java.util.Scanner;
+
 public class AppHw3 {
     public static void main(String[] args) {
-        greetings();
-        checkSign(1, 2, -33);
-        selectColor();
-        compareNumbers();
-        addOrSubtractAndPrint(1,2,true);
+//        greetings();
+//        checkSign(1, 2, -33);
+//        selectColor();
+//        compareNumbers();
+//        addOrSubtractAndPrint(1, 2, true);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число от 1 до 5");
+        int met = scanner.nextInt();
+        if (met == 1) {
+            greetings();
+        } else if (met == 2) {
+            checkSign(1, 2, -33);
+        } else if (met == 3) {
+            selectColor();
+        } else if (met == 4) {
+            compareNumbers();
+        } else if (met == 5) {
+            addOrSubtractAndPrint(1,2, true);
+        } else
+            System.out.println("Такого метода не существует");
+
     }
 
     public static void greetings() {
@@ -33,15 +51,17 @@ public class AppHw3 {
             System.out.println("Зеленый");
         }
     }
+
     public static void compareNumbers() {
         int a = 3;
         int b = 2;
-        if (a >= b){
+        if (a >= b) {
             System.out.println("a>=b");
         } else {
             System.out.println("a<b");
         }
     }
+
     public static void addOrSubtractAndPrint(int initValue, int delta, boolean increment) {
         if (increment) {
             System.out.println(initValue + delta);
@@ -49,4 +69,5 @@ public class AppHw3 {
             System.out.println(initValue - delta);
         }
     }
+
 }
