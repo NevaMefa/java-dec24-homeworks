683da4b3a45b271820d79ec57d622c614445dda9
Natali Korban <natafuuuck@gmail.com>
2025-01-15 21:52:10 +0300

    add greeting

--- src/main/java/ru/otus/java/basic/homeworks/homework3/AppHw3.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/AppHw3.java
@@ -2,8 +2,12 @@ package ru.otus.java.basic.homeworks.homework3;
 
 public class AppHw3 {
     public static void main(String[] args) {
-        int a =4;
-        System.out.println(a + 1);
-        System.out.println(a - 1);
+        greetings();
     }
+
+    public static void greetings() {
+        System.out.println("Hello \nWorld\nfrom \nJava");
+    }
+
+
 }
