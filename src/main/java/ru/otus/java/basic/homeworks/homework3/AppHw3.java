eb759b7d554d2f34aaaf27a10bea5ac2aaff4dbc
Natali Korban <natafuuuck@gmail.com>
2025-01-15 22:04:48 +0300

    add checkSign

--- src/main/java/ru/otus/java/basic/homeworks/homework3/AppHw3.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/AppHw3.java
@@ -3,11 +3,21 @@ package ru.otus.java.basic.homeworks.homework3;
 public class AppHw3 {
     public static void main(String[] args) {
         greetings();
+        checkSign(1, 2, -33);
     }
 
     public static void greetings() {
         System.out.println("Hello \nWorld\nfrom \nJava");
     }
 
+    public static void checkSign(int a, int b, int c) {
+        int sum = a + b + c;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+
+        }
+    }
 
 }
