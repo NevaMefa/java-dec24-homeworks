0307fd3365d756463ae2747792e9b0d18a8bba47
Natali Korban <natafuuuck@gmail.com>
2025-01-16 20:58:37 +0300

    поправлен ignore - убран target

--- src/main/java/ru/otus/java/basic/homeworks/homework3/AppHw3.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/AppHw3.java
@@ -21,16 +21,18 @@ public class AppHw3 {
         } else if (met == 4) {
             compareNumbers();
         } else if (met == 5) {
-            addOrSubtractAndPrint(1, 2, true);
+            addOrSubtractAndPrint();
         } else
             System.out.println("Такого метода не существует");
 
     }
 
+    // 1. Вывод приветствия
     public static void greetings() {
         System.out.println("Hello \nWorld\nfrom \nJava");
     }
 
+    //2. Метод проверки суммы чисел
     public static void checkSign(int a, int b, int c) {
         int sum = a + b + c;
         if (sum >= 0) {
@@ -41,20 +43,22 @@ public class AppHw3 {
         }
     }
 
+    //3. Метод выбора цвета
     public static void selectColor() {
-        int data = 15;
+        int data = (int) (Math.random() * 30);
         if (data <= 10) {
             System.out.println("Красный");
-        } else if (data > 10 && data <= 20) {
+        } else if (data <= 20) {
             System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
         }
     }
 
+    //4. Метод сравнения чисел
     public static void compareNumbers() {
-        int a = 3;
-        int b = 2;
+        int a = (int) (Math.random() * 10);
+        int b = (int) (Math.random() * 10);
         if (a >= b) {
             System.out.println("a>=b");
         } else {
@@ -62,7 +66,11 @@ public class AppHw3 {
         }
     }
 
-    public static void addOrSubtractAndPrint(int initValue, int delta, boolean increment) {
+    //5. Метод сложения или вычитания
+    public static void addOrSubtractAndPrint() {
+        int initValue = (int) (Math.random() * 100);
+        int delta = (int) (Math.random() * 50);
+        boolean increment = Math.random() < 0.5;
         if (increment) {
             System.out.println(initValue + delta);
         } else {
