f9b1d812b547255a6b8f8e9970ae467bb6103df2
Natali Korban <natafuuuck@gmail.com>
2025-01-22 18:34:35 +0300

    add numbNew

--- src/main/java/ru/otus/java/basic/homeworks/homework4/AppHw4.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework4/AppHw4.java
@@ -1,8 +1,12 @@
 package ru.otus.java.basic.homeworks.homework4;
 
+import java.util.Arrays;
+
 public class AppHw4 {
     public static void main(String[] args) {
-        first(4, "Привет!");
+        //first(4, "Привет!");
+        //sumNumbers(2, 5, 6, 7, 1, 1);
+        numbNew(3);
     }
 
     public static void first(int count, String text) {
@@ -10,5 +14,22 @@ public class AppHw4 {
             System.out.println(i + ". " + text);
     }
 
+    public static void sumNumbers(int... array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 5) {
+                sum += array[i];
+            }
+        }
+        System.out.println("Сумма:" + sum);
+    }
+
+    public static void numbNew(int number) {
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = number;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 
 }
