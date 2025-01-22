6493131d9eed518bde9eadd79f1b2753abefa4e1
Natali Korban <natafuuuck@gmail.com>
2025-01-22 16:54:00 +0300

    first tusk

--- src/main/java/ru/otus/java/basic/homeworks/homework4/AppHw4.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework4/AppHw4.java
@@ -2,11 +2,12 @@ package ru.otus.java.basic.homeworks.homework4;
 
 public class AppHw4 {
     public static void main(String[] args) {
-    first(4, "fffff");
+        first(4, "Привет!");
     }
 
-    public static void first(int a, String text) {
-        System.out.println(a + text);
+    public static void first(int count, String text) {
+        for (int i = 0; i < count; i++)
+            System.out.println(i + ". " + text);
     }
 
 
