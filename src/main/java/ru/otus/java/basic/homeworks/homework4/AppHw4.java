16f1fb0f7a91a1d82d4018c77e3ab4eab467c6b9
Natali Korban <natafuuuck@gmail.com>
2025-01-23 18:34:40 +0300

    Доавлен метод sumMet - 1е задание со звездочкой

--- src/main/java/ru/otus/java/basic/homeworks/homework4/AppHw4.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework4/AppHw4.java
@@ -11,6 +11,7 @@ public class AppHw4 {
         int[] arr1 = {8, 7, 7, 4, 5, 6};
         newNumbMassiv(5, arr1);
         compNumb(arr1);
+        sumMet();
     }
 
     public static void first(int count, String text) {
@@ -56,5 +57,17 @@ public class AppHw4 {
         } else {
             System.out.println("Сумма правой половины больше: " + sumRight);
         }
+    }
+
+    // заданиние со звездой
+    public static void sumMet() {
+        int[] met1 = {1, 2, 3};
+        int[] met2 = {3, 4, 5};
+        int[] met3 = {6, 7, 8};
+        int[] metSum = new int[met1.length + met2.length + met3.length];
+        for (int i = 0; i < met1.length; i++){
+            metSum[i] = met1[i] + met2[i] + met3[i];
+        }
+        System.out.println(Arrays.toString(metSum));
     }
 }
