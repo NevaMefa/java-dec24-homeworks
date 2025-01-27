31b9d1fe2aaefe62159e5ab4e8c99b938bc5c941
Natali Korban <natafuuuck@gmail.com>
2025-01-27 21:27:09 +0300

    Доавлен метод sumMet - 1е задание со звездочкой

--- src/main/java/ru/otus/java/basic/homeworks/homework4/AppHw4.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework4/AppHw4.java
@@ -4,18 +4,19 @@ import java.util.Arrays;
 
 public class AppHw4 {
     public static void main(String[] args) {
-        first(4, "Привет!");
-        sumNumbers(2, 5, 6, 7, 1, 1);
-        int[] arr = new int[3];
-        numbNew(4, arr);
-        int[] arr1 = {8, 7, 7, 4, 5, 6};
-        newNumbMassiv(5, arr1);
-        compNumb(arr1);
-        sumMet();
+//        first(4, "Привет!");
+//        sumNumbers(2, 5, 6, 7, 1, 1);
+//        int[] arr = new int[3];
+//        numbNew(4, arr);
+//        int[] arr1 = {8, 7, 7, 4, 5, 6};
+//        newNumbMassiv(5, arr1);
+//        compNumb(arr1);
+//        sumMet();
+        findEqual(5, 1, 1, 1, 1, 1);
     }
 
     public static void first(int count, String text) {
-        for (int i = 0; i < count; i++) {до
+        for (int i = 0; i < count; i++) {
             System.out.println(i + ". " + text);
         }
     }
@@ -66,9 +67,29 @@ public class AppHw4 {
         int[] met2 = {3, 4, 5};
         int[] met3 = {6, 7, 8};
         int[] metSum = new int[met1.length + met2.length + met3.length];
-        for (int i = 0; i < met1.length; i++){
+        for (int i = 0; i < met1.length; i++) {
             metSum[i] = met1[i] + met2[i] + met3[i];
         }
         System.out.println(Arrays.toString(metSum));
+    }
+
+    public static void findEqual(int... array) {
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum1 += array[i];
+        }
+        for (int i = 0; i < array.length - 1; i++) {
+            sum2 += array[i];
+            sum1 -= array[i];
+            if (sum2 == sum1) {
+                System.out.println("Равенство достигнуто между позициями " + i + " и " + (i + 1));
+                break;
+            } else {
+                if (i == array.length - 2) {
+                    System.out.println("Равенство не найдено");
+                }
+            }
+        }
     }
 }
