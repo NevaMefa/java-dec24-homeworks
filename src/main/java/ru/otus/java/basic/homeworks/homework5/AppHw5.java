79bd5dcee5bcf8614d18188116e22e8969380b49
Natali Korban <natafuuuck@gmail.com>
2025-01-29 15:09:32 +0300

    Добавлены вторая и третья  задачие из дз

--- src/main/java/ru/otus/java/basic/homeworks/homework5/AppHw5.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework5/AppHw5.java
@@ -2,17 +2,42 @@ package ru.otus.java.basic.homeworks.homework5;
 
 public class AppHw5 {
     public static void main(String[] args) {
-        int[][] mas2d = {{1, 1, 1}, {-1, -1, -1},{1, 1, 1}} ;
+        int[][] mas2d = {{1, 1, 1, 1}, {-1, -1, -1, -1}, {1, 1, 1, 1}};
+//        int result = sumOfPositiveElements(mas2d);
+//        System.out.println("Cymma " + result);
+//        box(2);
+        forNull(mas2d);
+    }
+
+    public static void forNull(int[][] mas2d) {
+        for (int i = 0; i < mas2d.length; i++) {
+            for (int j = 0; j < mas2d[i].length; j++) {
+                mas2d[i][i] = 0;
+                System.out.print(mas2d[i][j]);
+            }
+            System.out.println();
+        }
     }
 
     public static int sumOfPositiveElements(int[][] mas2d) {
         int sum = 0;
         for (int i = 0; i < mas2d.length; i++) {
-            if (mas2d[i][i] > 0) {
-                sum += mas2d[i][i];
+            for (int j = 0; j < mas2d[i].length; j++) {
+                if (mas2d[i][j] > 0) {
+                    sum += mas2d[i][j];
+                }
             }
-            System.out.println(sum);
         }
         return sum;
     }
+
+    public static void box(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
 }
