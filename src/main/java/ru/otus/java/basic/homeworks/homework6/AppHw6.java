df6e2cc2f83b14f8d8080c3263a0c6beda5cd527
Natali Korban <natafuuuck@gmail.com>
2025-02-04 22:40:51 +0300

    new branch for hard hw

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework6/AppHw6.java
@@ -0,0 +1,10 @@
+package ru.otus.java.basic.homeworks.homework6;
+
+public class AppHw6 {
+    public static void main(String[] args) {
+    User user1 = new User("Pypkin", "Vasya", "Alekseevich", 1998, "user1@test.ru");
+    user1.info();
+
+
+    }
+}
