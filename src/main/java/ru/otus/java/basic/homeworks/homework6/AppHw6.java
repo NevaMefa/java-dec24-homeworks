b91ac7188542cbcd1208d571ea202526a5f7b6ee
Natali Korban <natafuuuck@gmail.com>
2025-02-04 23:05:30 +0300

    Добавлен класс юзер и создан массив людей

--- src/main/java/ru/otus/java/basic/homeworks/homework6/AppHw6.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework6/AppHw6.java
@@ -2,9 +2,20 @@ package ru.otus.java.basic.homeworks.homework6;
 
 public class AppHw6 {
     public static void main(String[] args) {
-    User user1 = new User("Pypkin", "Vasya", "Alekseevich", 1998, "user1@test.ru");
-    user1.info();
+//    User user1 = new User("Pypkin", "Vasya", "Alekseevich", 1998, "user1@test.ru");
 
-
+        User[] users = {
+                new User("Pypkin", "Vasya", "Alekseevich", 1983, "user1@test.ru"),
+        new User("Sokolov", "Dmitri", "Dmitrievich", 1977, "user2@test.ru"),
+        new User("Shishkin", "Matvei", "Segreevich", 1976, "user3@test.ru"),
+        new User("Petrov", "Sergei", "Nikolaevich", 1975, "user4@test.ru"),
+        new User("Ivanov", "Aleksei", "Matveevich", 1963, "user5@test.ru"),
+        new User("Sidorov", "Viktor", "Vladimirovich", 1998, "user6@test.ru"),
+        new User("Zagirov", "Volodya", "Alekseevich", 1999, "user7@test.ru"),
+        new User("Belov", "Igor", "Nikolaevich", 1997, "user8@test.ru"),
+        new User("Svetlov", "Nikolai", "Gennadevich", 1996, "user9@test.ru"),
+        new User("Michailov", "Leonid", "Evgenevich", 1995, "user10@test.ru")};
     }
+
 }
+
