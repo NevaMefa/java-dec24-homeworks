37e48cf050f5d694317e0198a406bc4a8b1a4d6e
Natali Korban <natafuuuck@gmail.com>
2025-02-05 13:24:51 +0300

    Добавлен класс коробки

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework6hard/AppHwHard.java
@@ -0,0 +1,10 @@
+package ru.otus.java.basic.homeworks.homework6hard;
+
+public class AppHwHard {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/homework6hard/Box.java
@@ -0,0 +1,8 @@
+package ru.otus.java.basic.homeworks.homework6hard;
+
+public class Box {
+    public int size;
+    public String color;
+
+    }
+
