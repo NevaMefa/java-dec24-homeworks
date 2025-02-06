41b36090a857c7b54731630f373eba85afde3f2a
Natali Korban <natafuuuck@gmail.com>
2025-02-06 16:30:09 +0300

    nimiFix

--- src/main/java/ru/otus/java/basic/homeworks/homework6/AppHw6.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework6/AppHw6.java
@@ -11,7 +11,7 @@ public class AppHw6 {
                 new User("Sokolov", "Dmitri", "Dmitrievich", 1977, "user2@test.ru"),
                 new User("Shishkin", "Matvei", "Segreevich", 1976, "user3@test.ru"),
                 new User("Petrov", "Sergei", "Nikolaevich", 1975, "user4@test.ru"),
-                new User("Ivanov", "Aleksei", "Matveevich", 1963, "user5@test.ru"),
+                new User("Kuzmin", "Aleksei", "Matveevich", 1963, "user5@test.ru"),
                 new User("Sidorov", "Viktor", "Vladimirovich", 1998, "user6@test.ru"),
                 new User("Zagirov", "Volodya", "Alekseevich", 1999, "user7@test.ru"),
                 new User("Belov", "Igor", "Nikolaevich", 1997, "user8@test.ru"),
