ac7e6c349bcec7b2e1b81286030a89bde62ae6f0
Natali Korban <natafuuuck@gmail.com>
2025-02-08 10:31:09 +0300

    созданы классы и переменные в род.классе

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework7/AppHw7.java
@@ -0,0 +1,7 @@
+package ru.otus.java.basic.homeworks.homework7;
+
+public class AppHw7 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/homework7/animals/Animal.java
@@ -0,0 +1,8 @@
+package ru.otus.java.basic.homeworks.homework7.animals;
+
+public class Animal {
+    String name;
+    int speedRun;
+    int endurance;
+    int expEndurance;
+}
