d8c9f4f88a6db2ec2b451f0935f3604a84e9a591
Natali Korban <natafuuuck@gmail.com>
2025-02-13 19:46:54 +0300

    реализованы методы еды

--- src/main/java/ru/otus/java/basic/homeworks/homework8/AppHw8.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework8/AppHw8.java
@@ -2,6 +2,11 @@ package ru.otus.java.basic.homeworks.homework8;
 
 public class AppHw8 {
     public static void main(String[] args) {
-
+        Plate plate = new Plate(20);
+        Cat cat = new Cat("Murzik", 10);
+        plate.infoPlate();
+        cat.eat(plate);
+        cat.eat(plate);
+        cat.eat(plate);
     }
 }
