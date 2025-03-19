8b938db3ff02f476381e4854ad81653a086c6bcf
Natali Korban <natafuuuck@gmail.com>
2025-03-19 17:49:28 +0300

    Создан сервер и клиент и калькулятор

--- src/main/java/ru/otus/java/basic/homeworks/homework15/client/Client.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework15/client/Client.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    public static voi1d main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
