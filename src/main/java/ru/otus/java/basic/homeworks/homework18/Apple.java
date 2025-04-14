ca85eb1ae92d73761bfa232a826947845ee177ef
Natali Korban <natafuuuck@gmail.com>
2025-04-14 20:27:22 +0300

    Созданы коробки и методы для сравнения, добавления и пересыпания

--- src/main/java/ru/otus/java/basic/homeworks/homework18/Apple.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework18/Apple.java
@@ -3,6 +3,6 @@ package ru.otus.java.basic.homeworks.homework18;
 public class Apple extends Fruit {
     @Override
     public float getWeight() {
-        return 1.0f; // например, яблоко весит 1.0 условную единицу
+        return 1.0f;
     }
 }
--- src/main/java/ru/otus/java/basic/homeworks/homework18/Orange.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework18/Orange.java
@@ -3,6 +3,6 @@ package ru.otus.java.basic.homeworks.homework18;
 public class Orange extends Fruit {
     @Override
     public float getWeight() {
-        return 1.5f; // например, яблоко весит 1.0 условную единицу
+        return 1.5f;
     }
 }
